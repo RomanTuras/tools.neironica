ce7c18ee497e01ba795beb2e5198f97375142f4a
RomanTuras <salden.com@gmail.com>
2021-01-21 12:34:18 +0200

    exercise

--- resources/js/services/ApiServices.js
+++ resources/js/services/ApiServices.js
@@ -1,8 +1,8 @@
 import axios from "axios"
 
 const apiClient = axios.create({
-    // baseURL: `http://tools.neironica.com/`,
-    baseURL: `http://127.0.0.1:8000/`,
+    baseURL: `http://tools.neironica.com/`,
+    // baseURL: `http://127.0.0.1:8000/`,
     withCredentials: false, // This is the default
     headers: {
         Accept: "application/json",
